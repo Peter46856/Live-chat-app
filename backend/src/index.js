0ee5bc549d7103f70c682ad177269fb345beef2c
Peter Juma Mutiso <pjuma655@gmail.com>
2025-04-19 21:34:48 +0300

    modify index.js and socket.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://wondrous-gecko-51a026.netlify.app",
     credentials: true,
   })
 );
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://wondrous-gecko-51a026.netlify.app/login"],
   },
 });
 
