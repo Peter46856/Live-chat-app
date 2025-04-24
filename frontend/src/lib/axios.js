5a9593017d00b6e6b2c9eb3988a634caeea4d463
Peter Juma Mutiso <pjuma655@gmail.com>
2025-04-25 01:17:27 +0300

    Tailwind configuration modification

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-
+console.log("🧪 API Base URL:", import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // if you're using cookies/sessions
